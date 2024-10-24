bd3fe5c4d986a8fcd2caf517a823841f47a4b802
wriml92 <wriml92@knou.ac.kr>
2024-10-24 16:10:37 +0900

    Solution10.py

---
+++ Solution10.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    return sum(numbers) / len(numbers)
+
+# import numpy as np
+# def solution(numbers):
+#    return np.mean(numbers)
+
+# 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/120817
+# numbers의 원소의 평균값을 return
